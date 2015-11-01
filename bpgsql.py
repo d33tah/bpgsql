e7ae88bdf3c4801d644c9c3d24ea9887a733be65
Jacek Wielemborek <d33tah@gmail.com>
2015-11-02 00:04:23 +0100

    Print the args variable.

--- bpgsql.py
+++ bpgsql.py
@@ -523,6 +523,7 @@ class Connection(object):
             s.connect(args['host'])
         else:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print(args)
             s.connect((args['host'], int(args['port'])))
 
         if not args['user']:
